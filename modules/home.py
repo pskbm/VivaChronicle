180ab24ce657336d5c11ec3f20396e9ca2f0fad0
fsmosca <fsmosca@users.noreply.github.com>
2024-01-21 06:32:33 +0800

    Update home.py

--- modules/home.py
+++ modules/home.py
@@ -1,19 +1,10 @@
 """Suppports the home section in the main.py file"""
 
 
-CONTENT = '''A sample streamlit productivity app with non-native components. Its main purpose is to
-observe issues on authenticator component if there is.
+CONTENT = '''A sample streamlit productivity app that uses authenticator component.
 
 ### Features
 
-* Chat using openai LLM Model. You will provide your own openai key.
-* It has a sample todo app. It cannot be saved for now.
-
-### Components
-
-* The menu in the sidebar uses the Antd ui component. This is not a multi-page app.
-* The user authentication is provided by streamlit-authenticator component.
-* Users data are saved in secrets.toml file. This is not editable for now. You can
-use one registered user with username "boyet" and password "Racer" to login and see
-the page content.
+* Using streamlit's multi-page design.
+* It has a sample todo page. It cannot be saved for now.
 '''
