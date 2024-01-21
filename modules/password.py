326b16979c1b1cfbdad448d208a8d189647975a7
fsmosca <fsmosca@users.noreply.github.com>
2024-01-21 16:45:16 +0800

    Update password.py

--- modules/password.py
+++ modules/password.py
@@ -1,4 +1,20 @@
+import subprocess
+import sys
+
 import streamlit as st
+import pyperclip
+
+
+def copy_to_clipboard(text):
+    if sys.platform == "linux" or sys.platform == "linux2":
+        # Linux        
+        p = subprocess.Popen(['xclip', '-selection', 'c'], stdin=subprocess.PIPE)
+        p.communicate(input=text.encode('utf-8'))
+    elif sys.platform == "win32":
+        # Windows
+        pyperclip.copy(text)
+    else:
+        st.error("Clipboard support is not available for this OS.")
 
 
 def spacer(height: int = 30):
