0b307e53a69a6687cd8930dc4c1c3d30188a7b4f
fsmosca <fsmosca@users.noreply.github.com>
2024-01-21 16:53:53 +0800

    Update password.py

--- modules/password.py
+++ modules/password.py
@@ -6,15 +6,17 @@ import pyperclip
 
 
 def copy_to_clipboard(text):
-    if sys.platform == "linux" or sys.platform == "linux2":
-        # Linux        
-        p = subprocess.Popen(['xclip', '-selection', 'c'], stdin=subprocess.PIPE)
-        p.communicate(input=text.encode('utf-8'))
-    elif sys.platform == "win32":
-        # Windows
-        pyperclip.copy(text)
-    else:
-        st.error("Clipboard support is not available for this OS.")
+    pyperclip.copy(text)
+
+    # if sys.platform == "linux" or sys.platform == "linux2":
+    #     # Linux        
+    #     p = subprocess.Popen(['xclip', '-selection', 'c'], stdin=subprocess.PIPE)
+    #     p.communicate(input=text.encode('utf-8'))
+    # elif sys.platform == "win32":
+    #     # Windows
+    #     pyperclip.copy(text)
+    # else:
+    #     st.error("Clipboard support is not available for this OS.")
 
 
 def spacer(height: int = 30):
