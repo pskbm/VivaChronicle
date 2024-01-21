02c6302ae64f11e6254d46529df1a2853a104ad6
fsmosca <fsmosca@users.noreply.github.com>
2024-01-21 19:30:22 +0800

    Update Todo.py

--- pages/Todo.py
+++ pages/Todo.py
@@ -9,12 +9,10 @@ if 'authentication_status' not in st.session_state:
     st.session_state.authentication_status = None
 
 
-# Bring the user to the login page.
-if st.session_state.authentication_status is None:
-    st.switch_page('./pages/Authentication.py')
-
-
 st.title('Todo')
+
+if st.session_state.authentication_status is None:
+    st.error('Goto Authentication page to update login status.')
 
 if st.session_state.authentication_status:
     st.write(f'username: **{st.session_state.username}**')
