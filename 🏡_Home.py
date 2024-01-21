66779a49d3546aa54cdadbf8a8b47a4372ed96ce
fsmosca <fsmosca@users.noreply.github.com>
2024-01-21 18:04:50 +0800

    Create 🏡_Home.py

---
+++ 🏡_Home.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from modules.home import CONTENT
+
+
+st.set_page_config(
+    page_title='VivaChronicle',
+    layout='centered',
+    initial_sidebar_state='expanded',
+    page_icon=':art:',
+    menu_items={
+        'Report a bug': 'https://github.com/fsmosca/stauth-chat/issues'
+    }
+)
+
+
+if 'authentication_status' not in st.session_state:
+    st.session_state.authentication_status = None
+
+
+def main():
+    st.title('Home')
+
+    # Switch page
+    if st.session_state.authentication_status is None:
+        st.switch_page('./pages/Authentication.py')
+
+
+    if st.session_state["authentication_status"]:
+        st.markdown(CONTENT)
+
+
+if __name__ == '__main__':
+    main()
