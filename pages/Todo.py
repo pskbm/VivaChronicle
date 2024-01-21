13d31dc7edbd621131f0ab16a48c357fc4d4ab07
fsmosca <fsmosca@users.noreply.github.com>
2024-01-21 17:53:53 +0800

    Update Todo.py

--- pages/Todo.py
+++ pages/Todo.py
@@ -1,3 +1,6 @@
+"""Manages todo app."""
+
+
 import streamlit as st
 from modules.todo import Todo
 
@@ -6,7 +9,7 @@ if 'authentication_status' not in st.session_state:
     st.session_state.authentication_status = None
 
 
-# Switch page
+# Bring the user to the login page.
 if st.session_state.authentication_status is None:
     st.switch_page('./pages/Authentication.py')
 
