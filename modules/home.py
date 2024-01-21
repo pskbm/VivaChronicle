ecb9f442e83c077789161adc9fed65cc49814169
fsmosca <fsmosca@users.noreply.github.com>
2024-01-22 02:31:46 +0800

    Update home.py

--- modules/home.py
+++ modules/home.py
@@ -2,12 +2,13 @@
 
 
 CONTENT = '''A sample [streamlit](https://streamlit.io/) productivity app that
-uses [authenticator](https://github.com/mkhorasani/Streamlit-Authenticator) component. The main purpose
-is to check if the logged users can access its own content. This can be seen
+uses [authenticator](https://github.com/mkhorasani/Streamlit-Authenticator) component. The
+users info are currently hardcoded in the secret toml file in the cloud.
 
 ### Features
 
 * Using streamlit's multi-page design.
 * It has a sample todo page. The edited table cannot be saved for now.
 * It has a password random generator that anyone can access without the need for logging in.
+* It has a note page for taking notes. Anyone can access without the need for user authentication.
 '''
