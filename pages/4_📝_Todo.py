e9329d7b73d66006bbc440557b000cde0abc51b8
fsmosca <fsmosca@users.noreply.github.com>
2024-01-22 02:32:02 +0800

    Create 4_📝_Todo.py

---
+++ pages/4_📝_Todo.py
@@ -0,0 +1,24 @@
+"""Manages todo app."""
+
+
+import streamlit as st
+from modules.todo import Todo
+
+
+if 'authentication_status' not in st.session_state:
+    st.session_state.authentication_status = None
+
+
+st.title('Todo')
+
+if st.session_state.authentication_status is None:
+    st.error('''Goto Authentication page to log in or
+                update status and see the contents of this page.''')
+
+if st.session_state.authentication_status:
+    st.write(f'username: **{st.session_state.username}**')
+
+    todo = Todo(st.session_state.username)
+    todo.get_description()
+    todo.get_table()
+
