233b1e4dd73d46f7c215d67504ac880abea2ec0e
fsmosca <fsmosca@users.noreply.github.com>
2024-01-21 16:17:20 +0800

    Update password.py

--- modules/password.py
+++ modules/password.py
@@ -1,1 +1,6 @@
+import streamlit as st
 
+
+def spacer(height: int = 30):
+    """Used to align widgets horizontally."""
+    return st.write(f'<div style="height: {height}px;"></div>', unsafe_allow_html=True)
