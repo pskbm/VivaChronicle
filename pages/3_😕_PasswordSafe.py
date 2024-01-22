bab1fcd65e64a7acea3964f88fa523d83b28b4d3
fsmosca <fsmosca@users.noreply.github.com>
2024-01-22 20:25:38 +0800

    Update 3_😕_PasswordSafe.py

--- pages/3_😕_PasswordSafe.py
+++ pages/3_😕_PasswordSafe.py
@@ -6,35 +6,26 @@ The password generator uses this library:
 
 
 import streamlit as st
+from modules.password import get_password_generator, get_credential_vault
+from modules.auth import get_auth, get_login
 from passwordgenerator import pwgenerator
-from modules.password import spacer
 
 
 if 'pw' not in st.session_state:
     st.session_state.pw = pwgenerator.generate()
 
+st.title('Password Safe')
 
-def gen_pass_cb():
-    """Generates random password."""
-    st.session_state.pw = pwgenerator.generate()
+with st.container(border=True):
+    st.subheader('Password Generator')
+    st.markdown('Generates password randomly.')
+    get_password_generator()
 
+authenticator = get_auth()
+get_login(authenticator)
 
-st.title('Password Generator')
-
-st.markdown('Generates password randomly.')
-
-# Creates 2 widgets along the row.
-cols = st.columns([1, 5])
-
-with cols[0]:
-    spacer(height=29)
-    st.button('Generate', type='primary', on_click=gen_pass_cb)
-
-with cols[1]:
-    st.text_input(
-        'Random password',
-        value=st.session_state.pw,
-        key='rpw_k',
-        label_visibility='hidden'
-    )
-
+if st.session_state.authentication_status:
+    with st.container(border=True):
+        st.subheader('Credential Vault')
+        st.markdown('Store credentials.')
+        get_credential_vault()
