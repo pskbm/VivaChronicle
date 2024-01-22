5840e9899e3ddae628def324839c00c3e76f059b
fsmosca <fsmosca@users.noreply.github.com>
2024-01-22 21:32:28 +0800

    Update home.py

--- modules/home.py
+++ modules/home.py
@@ -8,7 +8,7 @@ users info are currently hardcoded in the secret toml file in the cloud.
 ### Features
 
 * Using streamlit's multi-page design.
-* It has a sample todo page. The edited table cannot be saved for now.
-* It has a password random generator that anyone can access without the need for logging in.
-* It has a note page for taking notes. Anyone can access without the need for user authentication.
+* It has a todo page.
+* It has a password page to stores credentials and password generator.
+* It has a note page for taking notes.
 '''
