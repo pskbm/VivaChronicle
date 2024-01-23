03edd0d73780abd00e37a707500bef71c8ade30c
fsmosca <fsmosca@users.noreply.github.com>
2024-01-24 02:14:20 +0800

    Add new user registration

--- modules/auth.py
+++ modules/auth.py
@@ -3,11 +3,15 @@
 
 import streamlit as st
 import streamlit_authenticator as stauth
-from modules.deta import get_config_from_deta
+from modules.deta import (get_config_from_deta, init_deta_drive,
+                          CONFIG_FN, dump_dict_to_yaml_stringio,
+                          save_config_to_deta)
 
 
-def get_auth():
-    config = get_config_from_deta()
+config = get_config_from_deta()
+
+
+def get_auth():    
     return stauth.Authenticate(
         config['credentials'],
         config['cookie']['name'],
@@ -21,7 +25,7 @@ def get_login(authenticator):
     authenticator.login('Login', 'main')
 
     if st.session_state["authentication_status"]:
-        pass  # st.write(f'Logged username: **{st.session_state.username}**')
+        st.write(f'Logged username: **{st.session_state.username}**')
 
     elif st.session_state["authentication_status"] is None:
         st.warning('Please enter your username and password')
@@ -33,12 +37,13 @@ def get_login(authenticator):
 def get_register(authenticator):
     try:
         if authenticator.register_user('Register user', 'main', preauthorization=False):
-            pass
+            save_config_to_deta(config)
+            st.success('Registration is successful!')
     except Exception as e:
         st.error(e)
 
 
 def get_logout(authenticator):
-    st.markdown(f'''Welcome **{st.session_state["name"]}** 	:thumbsup:''')
+    st.markdown(f'''Welcome **{st.session_state["name"]}**''')
     authenticator.logout('Logout', 'main', key='unique_key')
     
